bd5c21295c5455e7f86f236cc9eaa1bd333bac0b
NagaiH <nagai.h.ae@m.titech.ac.jp>
2015-04-12 02:50:43 +0900

    twitch sdk is now available

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,18 @@
 
 $(function () {
+  Twitch.init({clientId: 'ppbe2u8akeh60drxjevmpaxb310639v'}, function(error, status) {
+    // the sdk is now loaded
+  });
+  $('.twitch-connect').click(function() {
+    console.log("clicked!\n");
+ 
+  })
   $.getJSON("https://api.twitch.tv/kraken/streams/c9sneaky?callback=?",function(data){
     console.log(data);
+    if(data.stream == null){
+      $('#title').text("he is offline!\n");
+    }else{
+    $('#title').text(data.stream.game);
+    }
   });
 });
